c9e68a19b05058e44c0a0263d20791e079153d35
DoMinhHung <dominhhung04032003@gmail.com>
2026-03-07 21:28:41 +0700

    feat: add recommendation service with initial configuration and application setup

---
+++ recommendation-service/src/main/java/iuh/fit/se/recommendationservice/RecommendationServiceApplication.java
@@ -0,0 +1,18 @@
+package iuh.fit.se.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAsync
+@EnableScheduling
+public class RecommendationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
+}
