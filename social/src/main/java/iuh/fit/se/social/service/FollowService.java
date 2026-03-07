8eafaf806edf5adf218c897bca278af398fa2a67
DoMinhHung <dominhhung04032003@gmail.com>
2026-03-07 10:02:38 +0700

    feat(pom): remove social module from project structure

--- social/src/main/java/iuh/fit/se/social/service/FollowService.java
+++
@@ -1,9 +0,0 @@
-package iuh.fit.se.social.service;
-
-import iuh.fit.se.social.dto.response.ArtistSocialStatsResponse;
-import java.util.UUID;
-
-public interface FollowService {
-    void toggleFollow(UUID artistId);
-    ArtistSocialStatsResponse getArtistStats(UUID artistId);
-}
