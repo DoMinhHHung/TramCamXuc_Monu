625958e12789085be1afc03fed3d9de0289641f7
DoMinhHung <dominhhung04032003@gmail.com>
2026-04-16 16:19:47 +0700

    Remove waveform-related components and configurations from the project

--- music-service/src/main/java/iuh/fit/se/musicservice/MusicServiceApplication.java
+++ music-service/src/main/java/iuh/fit/se/musicservice/MusicServiceApplication.java
@@ -18,7 +18,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients(basePackages = "iuh.fit.se.musicservice.client")
 public class MusicServiceApplication {
     public static void main(String[] args) {
-        System.setProperty("java.awt.headless", "true");
         SpringApplication.run(MusicServiceApplication.class, args);
     }
 }
